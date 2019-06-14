3a3e3f72417559418cfaeef931494243ebc93066
naxola <naxola@hotmail.com>
2019-06-14 22:27:37 +0200

    layout menu responsiveness

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,45 @@
 import React, { Component } from "react";
+import "./header.css";
 
 class Header extends Component {
   state = {};
   render() {
-    return <p>Header</p>;
+    return (
+      <header>
+        <div className="container">
+          <div className="nav-toggle">
+            <span>Primero</span>
+            <span>Primero</span>
+            <span>Tercerp</span>
+          </div>
+          <a href="#" className="logo">
+            <img src="#" alt="Hayat logo" />
+          </a>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Serums</a>
+              </li>
+              <li>
+                <a href="#">Best Sellers</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+            </ul>
+          </nav>
+          <a href="#" className="cart">
+            Carrito
+            <span className="cart-icon" />
+            <span className="cart-counter" />
+          </a>
+        </div>
+
+        <div id="banner" className="banner">
+          Envíos gratuítos a toda la península
+        </div>
+      </header>
+    );
   }
 }
 
