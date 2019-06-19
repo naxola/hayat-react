672d988aac2159495e1df000967a898eb0da8c3c
naxola <naxola@hotmail.com>
2019-06-19 20:33:38 +0200

    added hero menu changed bad

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import HomePage from "./pages/home/home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
       <Footer />
     </div>
   );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+import "./home.css";
 
 class HomePage extends Component {
   render() {
     return (
-      <section>
-        <div className="hero">Pagina HOME</div>
-      </section>
+      <div className="wrapper">
+        <section className="hero">
+          <div className="inner">
+            <div className="clipper">
+              <h1>
+                <span>Made with Style </span> & Commitment
+              </h1>
+            </div>
+            <div className="anim-pannel">
+              <p>
+                Phosfluorescently engage worldwide methodologies with
+                web-enabled technology. Interactively coordinate proactive
+                e-commerce via process-centric “outside the box“ thinking.
+                Completely pursue scalable customer service through sustainable
+                potentialities.
+              </p>
+              <a href="#" className="read-btn primary">
+                comprar
+              </a>
+            </div>
+          </div>
+          <div className="image-hero">
+            <img src="#" alt="Serum Peptos" />
+          </div>
+        </section>
+      </div>
     );
   }
 }
